63ca53fded5583e78ccec512cc13d3374c5ea517
Ismael Antonio <ismael@pop-os.localdomain>
2021-02-03 18:49:32 +0300

    refactor the run method to a different file

--- bin/scraper.rb
+++ bin/scraper.rb
@@ -6,6 +6,27 @@ require 'httparty'
 require 'nokogiri'
 
 class InfoOutput < Scraper
+  def run
+    while page <= last_page && !flag
+      self.last_page = (total.to_f / start.count).round
+      doc.each do |movie|
+        self.film = {
+          title: movie.css('.qtip-title').text,
+          quality: movie.css('.jtip-quality').text,
+          year: movie.css('div.jt-info a').text.gsub(/\D/, ''),
+          url: movie.css('div.jtip-bottom a')[0].attributes['href'].value,
+          ratings: movie.css('div .jt-imdb').text
+        }
+        movies << film
+        prints
+      end
+      change_page
+      choice
+      Utilities.clear_terminal
+    end
+    movies
+  end
+
   def prints
     puts '=============================================================================='.light_blue
     puts " #{count} -  Movie:".light_blue + " #{film[:title]}  ||"
--- lib/config.rb
+++ lib/config.rb
@@ -19,27 +19,6 @@ class Scraper
     self.doc = parsed_page.css('div #hidden_tip')
   end
 
-  def run
-    while page <= last_page && !flag
-      self.last_page = (total.to_f / start.count).round
-      doc.each do |movie|
-        self.film = {
-          title: movie.css('.qtip-title').text,
-          quality: movie.css('.jtip-quality').text,
-          year: movie.css('div.jt-info a').text.gsub(/\D/, ''),
-          url: movie.css('div.jtip-bottom a')[0].attributes['href'].value,
-          ratings: movie.css('div .jt-imdb').text
-        }
-        movies << film
-        prints
-      end
-      change_page
-      choice
-      Utilities.clear_terminal
-    end
-    movies
-  end
-
   def choice
     case command
     when 'exit'
