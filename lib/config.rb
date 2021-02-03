3bcfb883c1ed77b50e9772d7964808445bbb4f16
Ismael Antonio <ismael@pop-os.localdomain>
2021-02-03 04:58:37 +0300

    broke functionality of run into parts

--- lib/config.rb
+++ lib/config.rb
@@ -1,5 +1,5 @@
 class Scraper
-  attr_accessor :page, :url, :p_url, :films_per_page, :last_page, :movies, :flag, :total, :command, :count, :doc
+  attr_accessor :page, :url, :p_url, :films_per_page, :last_page, :movies, :flag, :total, :command, :count, :doc, :film
 
   def initialize
     self.page = 1
@@ -8,16 +8,27 @@ class Scraper
     self.movies = []
     self.flag = false
     self.count = 1
+    self.film = {}
   end
 
-  private
-
-  def start
-    self.url = 'https://gostream.site/123movies/'
-    self.p_url = url + "page/#{page}"
-    puts "\n"
-    parsed_page = Nokogiri::HTML(HTTParty.get(p_url).body)
-    self.doc = parsed_page.css('div #hidden_tip')
+  def run
+    while page <= last_page && !flag
+      self.last_page = (total.to_f / start.count).round
+      doc.each do |movie|
+        self.film = {
+          title: movie.css('.qtip-title').text,
+          quality: movie.css('.jtip-quality').text,
+          year: movie.css('div.jt-info a').text.gsub(/\D/, ''),
+          url: movie.css('div.jtip-bottom a')[0].attributes['href'].value,
+          ratings: movie.css('div .jt-imdb').text
+        }
+        movies << film
+        prints
+      end
+      change_page
+      choice
+      Utilities.clear_terminal
+    end
   end
 
   def choice
